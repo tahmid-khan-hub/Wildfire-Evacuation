8a9c71a3aff54639a62e5884dde6963f7e6ba299
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-07-20 22:52:57 +0600

    Fix the errors of fire_spread

--- env/fire_spread.py
+++ env/fire_spread.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from env.constants import ( GRID_WIDTH, GRID_HEIGHT, DANGEROUS_FIRE_STATES, FIREPROOF_TERRAIN, FIRE_LOW, IGNITION_PROB, BURN_DURATION, FIRE_ASH, FIRE_HIGH, FIRE_MID )
+from env.constants import ( GRID_WIDTH, GRID_HEIGHT, DANGEROUS_FIRE_STATES, FIREPROOF_TERRAIN, FIRE_LOW, IGNITION_PROB, BURN_DURATION, FIRE_ASH, FIRE_HIGH, FIRE_MID, FIRE_NONE )
 
 # 8-directional neighborhood
 NEIGHBOR_OFFSETS = [
@@ -29,13 +29,13 @@ def _try_ignite_neighbors(terrain, fire_state, new_fire_state, new_burn_timer, r
     for (y, x) in burning_cells:
         for dx, dy in NEIGHBOR_OFFSETS:
             nx, ny = x + dx, y + dy
-            if not 0 <= nx < GRID_WIDTH and 0 <= ny <GRID_HEIGHT:
+            if not (0 <= nx < GRID_WIDTH and 0 <= ny < GRID_HEIGHT):
                 continue # checking whether the neighbor cells is inside the grid or not
 
             neighbor_terrain = terrain[ny, nx]
             if neighbor_terrain in FIREPROOF_TERRAIN:
                 continue # if the neighbor cell is fire proof then skip
-            if fire_state[ny, nx] != FIRE_LOW:
+            if fire_state[ny, nx] != FIRE_NONE:
                 continue # already burning or ash
 
             prob = IGNITION_PROB[neighbor_terrain]
@@ -44,7 +44,7 @@ def _try_ignite_neighbors(terrain, fire_state, new_fire_state, new_burn_timer, r
                 new_burn_timer[ny, nx] = 0 # for fire_low the value is set as 0
 
 def _advance_burning_cells(terrain, fire_state,  new_fire_state, new_burn_timer):
-    ys, xs = np.where(np.isin(fire_state, list(NEIGHBOR_OFFSETS)))
+    ys, xs = np.where(np.isin(fire_state, list(DANGEROUS_FIRE_STATES)))
 
     for i in range(len(ys)):
         y = ys[i]
@@ -56,11 +56,11 @@ def _advance_burning_cells(terrain, fire_state,  new_fire_state, new_burn_timer)
         third = max(1, duration // 3) # to get int value and timer at least 1 (min)
         elapsed = new_burn_timer[y, x]
 
-        if(elapsed >= duration):
+        if elapsed >= duration:
             new_fire_state[y, x] = FIRE_ASH # if the burn time is greater than the duration then it already burnt - become ash
-        elif(elapsed >= 2*third):
+        elif elapsed >= 2*third:
             new_fire_state[y, x] = FIRE_HIGH # if duration = 12 and third = 4 then 0-low, 4-mid, 8-high, 12-ash. to get the high we have to pass from mid and low third means 2 level - thats why *2
-        elif(elapsed >= third):
+        elif elapsed >= third:
             new_fire_state[y, x] = FIRE_MID 
         else:
             new_fire_state[y, x] = FIRE_LOW # means < third is low
