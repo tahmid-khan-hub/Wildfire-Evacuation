9453b56856094601b7cce08ae11162d679546a93
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-07-21 00:14:47 +0600

    Create test_step_does_not_crash function inside test_env

--- tests/test_env.py
+++ tests/test_env.py
@@ -10,3 +10,15 @@ def test_reset_returns_valid_obs():
     assert not np.any(np.isnan(obs)) # checking whether a Nan is present or not - if yes then assertion error 
     # (not used because if it is false then not will reverse it to true and assertion error will not be thrown if it is true - here false means no Nan present in obs)
 
+# calling step() with random actions, the environment should not crash and should always return valid data
+def test_step_does_not_crash():
+    env = EmberPathEnv(seed=3)
+    env.reset() # new episode
+    for _ in range(50): # upto 50 times
+        action = env.action_space.sample() # here, sample choose any random action value
+        obs, reward, terminated, truncated, info = env.step(action)
+        assert obs.shape == env.observation_space.shape # if the shape changes then assertion error
+        assert isinstance(reward, float) # checking whether the reward is in float or not
+        if terminated or truncated:
+            break # if terminated or truncated happen then stop the loop
+
